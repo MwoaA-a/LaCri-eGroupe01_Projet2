bd0c719b5412609951168057220c39afcc33f8fd
Boisson Thomas <thomasboissontb30@gmail.com>
2023-11-29 16:48:22 +0100

    Route and html creation
    Création des pages HTML et des routes :
    -Mon compte
    -Facturation
    -Lots
    
    Et ajout des route à la NavBar :
    -Accueil (sur le logo)
    -mon compte
    -facture
    -lots

--- src/Controller/AccueilControler.php
+++ src/Controller/AccueilControler.php
@@ -8,10 +8,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilControler extends AbstractController
 {
-    #[Route('/accueil', name: 'app_accueil')]
+    #[Route('', name: 'app_accueil')]
     public function index(): Response
     {
         return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilControler',
+        ]);
+    }
+
+    #[Route('/Lots', name: 'app_Lots')]
+    public function Lots(): Response
+    {
+        return $this->render('accueil/Lots.html.twig', [
+            'controller_name' => 'AccueilControler',
+        ]);
+    }
+
+    #[Route('/facture', name: 'app_facture')]
+    public function facturation(): Response
+    {
+        return $this->render('accueil/facturation.html.twig', [
+            'controller_name' => 'AccueilControler',
+        ]);
+    }
+
+    #[Route('/mon-compte', name: 'app_compte')]
+    public function compte(): Response
+    {
+        return $this->render('accueil/compte.html.twig', [
             'controller_name' => 'AccueilControler',
         ]);
     }
